72815624a46c9c935f5f7a811e166f703d09de3f
Hamza Zaheer <malikhamza1619619@gmail.com>
2022-08-05 20:30:30 +0500

    2 Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import User from "./user/pages/User";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/*Switch ke jagha Routes use hota ha in v6*/}
+        <Route path="/" element={<User />} />
+        <Route path="/places/new" element={<NewPlace />} />
+
+        {/*Redirect ke jagha Navigate use hota ha in v6*/}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
